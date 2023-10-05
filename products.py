a863c72f5fc7c1398872583aeb28477250a1bde1
“terrytao” <color1252002@gmail.com>
2023-10-05 17:01:01 +0800

    增加項目欄位和encodingutf-8

--- products.py
+++ products.py
@@ -26,9 +26,10 @@ for p in products :
 	print(p[0], '的價格為', p[1]) #用小清單的位置列出商品和價格
 
 
-# class 63
+# class 63 , 65(+標題和編碼)
 # 商品寫入 #w
 
-with open ('products.csv', 'w') as f : #txt/csv
+with open ('products.csv', 'w', encoding = 'utf-8' ) as f : #txt/csv
+	f.write('商品,價格\n')#加入欄位名稱
 	for p in products :
 		f.write(p[0] + ',' + p[1] + '\n') # ',' 是為了檔案可視性分隔和excel 打開方便
